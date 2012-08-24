5db3a4a2ccbf1bf5a3ff2f2b46278a8353dc43f5
Owen Johnson <owen@owenjohnson.info>
2012-08-24 14:40:18 -0500

    Hacked stuff together
    Have filtering and multisorting working. This should be a good start for
    everything else.

---
+++ Helpers.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Windows.Data;
+using Microsoft.Windows.Controls;
+
+namespace Labs.Filtering
+{
+    public static class Helpers
+    {
+        public static string GetSortMemberPath(DataGridColumn column)
+        {
+            // find the sortmemberpath
+            string sortPropertyName = column.SortMemberPath;
+            if (string.IsNullOrEmpty(sortPropertyName))
+            {
+                DataGridBoundColumn boundColumn = column as DataGridBoundColumn;
+                if (boundColumn != null)
+                {
+                    Binding binding = boundColumn.Binding as Binding;
+                    if (binding != null)
+                    {
+                        if (!string.IsNullOrEmpty(binding.XPath))
+                        {
+                            sortPropertyName = binding.XPath;
+                        }
+                        else if (binding.Path != null)
+                        {
+                            sortPropertyName = binding.Path.Path;
+                        }
+                    }
+                }
+            }
+
+            return sortPropertyName;
+        }
+
+        public static int FindSortDescription(SortDescriptionCollection sortDescriptions, string sortPropertyName)
+        {
+            int index = -1;
+            int i = 0;
+            foreach (SortDescription sortDesc in sortDescriptions)
+            {
+                if (string.Compare(sortDesc.PropertyName, sortPropertyName) == 0)
+                {
+                    index = i;
+                    break;
+                }
+                i++;
+            }
+
+            return index;
+        }
+    }
+}
